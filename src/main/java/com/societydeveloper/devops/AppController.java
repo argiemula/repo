4197f48bb44f2d4a7787f8988be2866146fbae79
Argie <sd.argie.mula.com>
2022-02-03 23:18:02 +0800

    Modified mapping

--- src/main/java/com/societydeveloper/devops/AppController.java
+++ src/main/java/com/societydeveloper/devops/AppController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class AppController {
 
-	@GetMapping("/index")
+	@GetMapping("/")
 	public String home() {
 		return "index";
 	}
