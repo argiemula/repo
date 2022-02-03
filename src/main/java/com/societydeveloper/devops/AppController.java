a7f93987342b78001a906b6e90db313d29900443
Argie <sd.argie.mula.com>
2022-02-04 00:25:12 +0800

    modified controller to rest

--- src/main/java/com/societydeveloper/devops/AppController.java
+++ src/main/java/com/societydeveloper/devops/AppController.java
@@ -2,8 +2,9 @@ package com.societydeveloper.devops;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class AppController {
 
 	@GetMapping("/")
