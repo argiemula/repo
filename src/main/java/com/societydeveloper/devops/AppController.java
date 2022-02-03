bf496c5214d5cf5d77696a101d1f9b7c94d9e726
Argie <sd.argie.mula.com>
2022-02-04 00:30:18 +0800

    f

--- src/main/java/com/societydeveloper/devops/AppController.java
+++ src/main/java/com/societydeveloper/devops/AppController.java
@@ -2,9 +2,8 @@ package com.societydeveloper.devops;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class AppController {
 
 	@GetMapping("/")
