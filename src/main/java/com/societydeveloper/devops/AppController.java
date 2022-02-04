3d1b2576a4311c9c8373daaafda202a72614bb28
Argie <sd.argie.mula.com>
2022-02-04 10:35:58 +0800

    modified to request mapping

--- src/main/java/com/societydeveloper/devops/AppController.java
+++ src/main/java/com/societydeveloper/devops/AppController.java
@@ -2,8 +2,10 @@ package com.societydeveloper.devops;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/home")
 public class AppController {
 
 	@GetMapping("/")
