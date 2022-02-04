c3b7321015ab6f1cbc6aaef68328fd3d8f8d6772
Argie <sd.argie.mula.com>
2022-02-04 10:44:32 +0800

    extends in main app

--- src/main/java/com/societydeveloper/devops/AppDeployApplication.java
+++ src/main/java/com/societydeveloper/devops/AppDeployApplication.java
@@ -2,9 +2,10 @@ package com.societydeveloper.devops;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AppDeployApplication {
+public class AppDeployApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(AppDeployApplication.class, args);
