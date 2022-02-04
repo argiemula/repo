ab49d45a6bb9e5efeb287fbbf21701429d3c1866
Argie <sd.argie.mula.com>
2022-02-04 10:51:03 +0800

    modified main

--- src/main/java/com/societydeveloper/devops/AppDeployApplication.java
+++ src/main/java/com/societydeveloper/devops/AppDeployApplication.java
@@ -2,10 +2,18 @@ package com.societydeveloper.devops;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class AppDeployApplication extends SpringBootServletInitializer{
+
+	
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(AppDeployApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(AppDeployApplication.class, args);
